069b86c5670f2d5e7f242c8a9ea369a8bba56a10
MiladNazeri <milad@miladnazeri.com>
2016-09-22 18:32:01 -0400

    trying ali

--- browser/js/milad/milad.controller.js
+++ browser/js/milad/milad.controller.js
@@ -4,29 +4,12 @@ app.controller("MiladControl", function($scope, inputFactory){
     $("#myVideo").css({
         "transform": "rotateY(180deg)"
     })
-    //TESTING INTERPOLATE FUNCTION
 
+    //TESTING INTERPOLATE FUNCTION
+    var input = new inputFactory.Input(0,300,0,300);
     //GETTING COLORS TO BE TRACKED
     // var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
     var colors = new tracking.ColorTracker(['yellow']);
-
-   //CREATING INPUT OBJECT TO SEND TO RENDER
-    var input = {
-        x: 0,
-        y: 0
-    };
-    input.getX = function(){
-        return this.x
-    };
-    input.setX = function(x){
-        this.x = x
-    };
-    input.getY = function(){
-        return this.x
-    };
-    input.setY = function(x){
-        this.x = x
-    };
 
     //ASSIGNING LISTENERS FROM COLOR TRACK
     colors.on('track', function(event) {
@@ -35,6 +18,8 @@ app.controller("MiladControl", function($scope, inputFactory){
       } else {
         event.data.forEach(function(rect) {
             input.setX(rect.x)
+            input.setY(rect.y)
+            input.setZ(rect.width)
 
           console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
         });
@@ -43,6 +28,7 @@ app.controller("MiladControl", function($scope, inputFactory){
 
     //PUTTING QUERY ON myVIDEO
     var video = document.querySelector("#myVideo");
+    var myCubes = document.querySelector("#myCubes");
 
     //TRACKING MY VIDEO
     tracking.track('#myVideo', colors)
@@ -58,26 +44,32 @@ app.controller("MiladControl", function($scope, inputFactory){
 
     //3JS
         var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 75, (window.innerWidth/3)/(window.innerHeight/3), 0.1, 1000 );
+        var camera = new THREE.PerspectiveCamera( 75, (window.innerWidth)/(window.innerHeight), 0.1, 1000 );
 
-        console.log("innerWidth",window.innerWidth/3)
-        console.log("innerHeight",window.innerHeight/3)
+        console.log("innerWidth",window.innerWidth)
+        console.log("innerHeight",window.innerHeight)
 
         var renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth/3, window.innerHeight/3 );
-        document.body.appendChild( renderer.domElement );
+        renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+        myCubes.appendChild( renderer.domElement );
+
+        $("#myCubes canvas").css({
+            "transform": "rotateY(180deg)"
+        })
 
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         var cube = new THREE.Mesh( geometry, material );
         scene.add( cube );
 
-        camera.position.z = 0;
+        camera.position.z = 20;
 
         var render = function () {
             requestAnimationFrame( render );
-            console.log("input of input get x", input.getX())
-            cube.position.x = input.getX();
+            console.log("input of input get x", input.getX(-25,25))
+            cube.position.x = input.getX(-5,5);
+            // cube.position.y = input.getY(-50,50);
+            // cube.position.z = input.getZ(-50,50);
             // cube.position.y = input.getX();
 
 
